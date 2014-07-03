e4a4292005e60f62095c4fd0752ead59af338f9c
zhoushineyoung <zhoushineyoung@hotmail.com>
2014-07-03 14:01:57 +0800

    modified currency-type load style, add the very tester

--- src/main/java/com/ebj/domain/AllCurrencies.java
+++ src/main/java/com/ebj/domain/AllCurrencies.java
@@ -6,65 +6,43 @@ import java.util.Set;
 
 import com.ebj.conf.ConfigManager;
 import com.ebj.conf.ConfigurationSchema;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Sets;
 
 public class AllCurrencies {
-	private static Set<String> currencyNames = null;
-	private static String[] allCurrencyName = null;
-	private static String[] codes = {"USD", "JPY", "BGN", "CZK", "DKK", "GBP", "HUF", "LTL",
-		"LVL", "PLN", "RON", "SEK", "CHF", "NOK", "HRK", "RUB",
-		"TRY", "AUD", "BRL", "CAD", "CNY", "HKD", "IDR", "ILS",
-		"INR", "KRW", "MXN", "MYR", "NZD", "PHP", "SGD", "THB",
-		"ZAR", "EUR", "EUR", "USD"};
-	
-	static{
-		// 读取配置文件，动态加载java代码片段  eg. static String[] codes = {"USD", "JPY", "BGN", "CZK"...}
-//	    ConfigurationSchema config = ConfigManager.getInstance().getConfig();
-//        dataTableHost = config.getH2ServerHost();
-//        dataTablePort = config.getH2ServerPort();
+    private static Set<String> currenciesNameSet = null;
+    private static String[] allCurrencyName = null;
+    private static String[] codes = {
+            "CNY", "LVL", "JPY", "RON", "CZK", "MXN", "CAD", "ZAR", "NZD", "AUD", "GBP", "ILS", "NOK", "CHF", "RUB", "INR", "THB", "IDR", "TRY", "SGD", "HKD",
+            "LTL", "HRK", "DKK", "MYR", "SEK", "BRL", "EUR", "BGN", "PHP", "HUF", "PLN", "USD", "KRW" };
 
-	    currencyNames = new HashSet<String>(Arrays.asList(codes));
-	}
-	
-	public static void setCurrencyNames(String ...names) {
-	    codes = names;
-		if (codes.length > 0) {
-			for (String string : codes) {
-				System.out.println(string.getClass().getName() + " - " + string.toString());
-			}
-			currencyNames = new HashSet<String>(Arrays.asList(codes));
-		}
-	}
-	
-	public static String[] getAllCurrencyNames() {
-		int m = currencyNames.size();
-		if (m > 0) {
-			allCurrencyName = currencyNames.toArray(new String[0]);
-			return allCurrencyName;
-		}
-		return null;
-	}
-	
-	public static void main(String[] args) {
-//	    System.out.println(codes.length);
-//		Set<String> names = new HashSet<String>(Arrays.asList(codes));
-//		System.out.println(names.size());
-//		String[] newNames = names.toArray(new String[0]);
-//		System.out.println(newNames.length);
-//		for (String string : newNames) {
-//            System.out.print(string + "-");
-//        }
-	    for (String string : AllCurrencies.getAllCurrencyNames()) {
-            System.out.println(string);
+    static {
+        // load currency-type configuration from properties
+        ConfigurationSchema config = ConfigManager.getInstance().getConfig();
+        Iterable<String> currenciesName = Splitter.on(",").omitEmptyStrings().split(config.getCurrenciesName());
+        if (null != currenciesName) {
+            currenciesNameSet = Sets.newHashSet(currenciesName);
+        } else {
+            currenciesNameSet = new HashSet<String>(Arrays.asList(codes));
         }
-	    AllCurrencies.setCurrencyNames(new String[]{"ABC","DEF","HIJ"});
-	    for (String string : AllCurrencies.getAllCurrencyNames()) {
-            System.out.println(string);
+    }
+
+    public static void setCurrencyNames(String... names) {
+        codes = names;
+        if (codes.length > 0) {
+            for (String string : codes) {
+                System.out.println(string.getClass().getName() + " - " + string.toString());
+            }
+            currenciesNameSet = new HashSet<String>(Arrays.asList(codes));
         }
-	    
-	    String[] newArr2 = {"KKK","LLL","MMM"};
-	    AllCurrencies.setCurrencyNames(newArr2);
-	    for (String string : AllCurrencies.getAllCurrencyNames()) {
-	        System.out.println(string);
-	    }
-	}
+    }
+
+    public static String[] getAllCurrencyNames() {
+        int m = currenciesNameSet.size();
+        if (m > 0) {
+            allCurrencyName = currenciesNameSet.toArray(new String[0]);
+            return allCurrencyName;
+        }
+        return null;
+    }
 }
